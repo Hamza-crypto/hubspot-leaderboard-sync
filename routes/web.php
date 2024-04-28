141249b8fb5acb9fe7e40bd89e0fd861618293e8
Hamza <6793siddique@gmail.com>
2024-04-28 12:19:05 +0500

    Added missing controler

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LeaderboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Artisan;
