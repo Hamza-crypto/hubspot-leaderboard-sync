bde580185c070c54a846f30a9b1b2b3be5d087e8
Hamza <6793siddique@gmail.com>
2024-04-29 10:54:40 +0500

    Comment unnecessary libraries

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
     <link class="js-stylesheet" href="{{ asset('assets/css/light.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css"> --}}
     @yield('styles')
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
@@ -47,10 +47,10 @@
         })
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.2/sweetalert2.all.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script> --}}
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.2/sweetalert2.all.min.js"></script> --}}
     <script src="{{ asset('assets/js/alerts.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.doubleScroll.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/jquery.doubleScroll.js') }}"></script> --}}
 
     @yield('alert')
     @yield('scripts')
