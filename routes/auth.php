d71a01571a2b70af5e3b32d40ba54bf5e0bca703
Hamza <6793siddique@gmail.com>
2024-05-10 07:41:17 +0500

    Enabled registration

--- routes/auth.php
+++ routes/auth.php
@@ -12,11 +12,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    // Route::get('register', [RegisteredUserController::class, 'create'])
-    //             ->name('register');
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
 
 
-    Route::view('register', '/welcome');
+    // Route::view('register', '/welcome');
 
 
     Route::post('register', [RegisteredUserController::class, 'store']);
