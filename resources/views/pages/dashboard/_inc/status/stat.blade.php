a084a036750f8aedd91933a1cee582ebc8cadd18
Hamza <6793siddique@gmail.com>
2024-06-21 13:57:37 +0500

    Added new stat widget for other CRM

--- resources/views/pages/dashboard/_inc/status/stat.blade.php
+++ resources/views/pages/dashboard/_inc/status/stat.blade.php
@@ -141,7 +141,7 @@
 
 <div class="row">
 
-    <div class="col-12 col-sm-6 col-xxl d-flex">
+    {{-- <div class="col-12 col-sm-6 col-xxl d-flex">
         <div class="card flex-fill">
             <div class="card-body py-4">
                 <div class="media">
@@ -162,9 +162,9 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
-    <div class="col-12 col-sm-6 col-xxl d-flex">
+    {{-- <div class="col-12 col-sm-6 col-xxl d-flex">
         <div class="card flex-fill">
             <div class="card-body py-4">
                 <div class="media">
@@ -185,7 +185,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <div class="col-12 col-sm-6 col-xxl d-flex">
         <div class="card flex-fill">
@@ -194,6 +194,30 @@
                     <div class="media-body">
                         <h3 class="mb-2"> <span id="clock_exp_notice"></span> </h3>
                         <p class="mb-2">CLOCK EXP NOTICE</p>
+                    </div>
+                    <div class="d-inline-block ml-3">
+                        <div class="stat">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-activity align-middle">
+                                <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-12 col-sm-6 col-xxl d-flex">
+        <div class="card flex-fill">
+            <div class="card-body py-4">
+                <div class="media">
+                    <div class="media-body">
+                        <h3 class="mb-2"> <span id="today_deals_other_hubspot"></span> </h3>
+                        <p class="mb-2">ACA Helpline Today deals</p>
                     </div>
                     <div class="d-inline-block ml-3">
                         <div class="stat">
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -93,9 +93,25 @@
             });
         }
 
+        function fetchDataForOtherHubspot() {
+
+            $.ajax({
+                url: 'https://ncaleaderboard.com/api/stats/today_deals',
+                method: 'GET',
+                dataType: 'json',
+                success: function(response) {
+                    populateWidgets(response);
+                },
+                error: function() {
+                    console.log('Failed to fetch stats from other huspot API.');
+                }
+            });
+        }
+
 
         $(document).ready(function() {
             fetchData();
+            fetchDataForOtherHubspot();
         });
     </script>
 @endsection
